e5d4ccd3bbdd0710539013dd529af9ca078ad5ca
Alexander <alex.goryushkin@gmail.com>
2018-10-16 17:22:25 +0500

    Горюшкин Александр. Задание 05 фикс

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,7 +1,6 @@
 package com.example.task05;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Ломаная линия
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -4,6 +4,7 @@ public class Task05Main {
     public static void main(String[] args) {
         Point[] points = {new Point(1, 2), new Point(2, 2), new Point(5, 5.999)};
         PolygonalLine line = new PolygonalLine(points);
+        line.setPoints(points);
         double expected = line.getLength();
         System.out.println(expected);
     }
